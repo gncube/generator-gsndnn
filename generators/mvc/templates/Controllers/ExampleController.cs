5a58f2b73732bad47a4acf07236d2884a8eb2a27
Gerald Ncube <gncube@gmail.com>
2019-12-28 10:37:08 +0000

    remove whitescpae

--- generators/mvc/templates/Controllers/ExampleController.cs
+++ generators/mvc/templates/Controllers/ExampleController.cs
@@ -1,4 +1,4 @@
-using DotNetNuke.Framework.JavaScriptLibraries;
+﻿using DotNetNuke.Framework.JavaScriptLibraries;
 using DotNetNuke.Web.Mvc.Framework.ActionFilters;
 using DotNetNuke.Web.Mvc.Framework.Controllers;
 using System;
@@ -13,56 +13,56 @@ namespace <%= fullNamespace %>.Controllers
     public class <%= extensionName %>Controller : DnnController
     {
 
-        public ActionResult Delete(int itemId)
-        {
-                <%= extensionName %>InfoRepository.Instance.DeleteItem(itemId, ModuleContext.ModuleId);
-                return RedirectToDefaultRoute();
-        }
+      public ActionResult Delete(int itemId)
+      {
+          <%= extensionName %>InfoRepository.Instance.DeleteItem(itemId, ModuleContext.ModuleId);
+          return RedirectToDefaultRoute();
+      }
 
 
-    public ActionResult Edit(int itemId = -1)
-    {
-        DotNetNuke.Framework.JavaScriptLibraries.JavaScript.RequestRegistration(CommonJs.DnnPlugins);
+      public ActionResult Edit(int itemId = -1)
+      {
+          DotNetNuke.Framework.JavaScriptLibraries.JavaScript.RequestRegistration(CommonJs.DnnPlugins);
 
-        var item = (itemId == -1)
+          var item = (itemId == -1)
             ? new <%= extensionName %>Info { ModuleId = ModuleContext.ModuleId }
-                                        : <%= extensionName %>InfoRepository.Instance.GetItem(itemId, ModuleContext.ModuleId);
+            : <%= extensionName %>InfoRepository.Instance.GetItem(itemId, ModuleContext.ModuleId);
 
-        return View(item);
-    }
+          return View(item);
+      }
 
-    [HttpPost]
-    [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
-    public ActionResult Edit(<%= extensionName %>Info item)
-    {
-        if (item.<%= extensionName %> Id == -1)
-        {
-            item.CreatedByUserId = User.UserID;
-            item.CreatedOnDate = DateTime.UtcNow;
-            item.LastUpdatedByUserId = User.UserID;
-            item.LastUpdatedOnDate = DateTime.UtcNow;
+      [HttpPost]
+      [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
+      public ActionResult Edit(<%= extensionName %>Info item)
+      {
+          if (item.<%= extensionName %>Id == -1)
+          {
+              item.CreatedByUserId = User.UserID;
+              item.CreatedOnDate = DateTime.UtcNow;
+              item.LastUpdatedByUserId = User.UserID;
+              item.LastUpdatedOnDate = DateTime.UtcNow;
 
-                                        <%= extensionName %>InfoRepository.Instance.CreateItem(item);
-        }
-        else
-        {
-            var existingItem = <%= extensionName %>InfoRepository.Instance.GetItem(item.<%= extensionName %> Id, item.ModuleId);
-            existingItem.LastUpdatedByUserId = User.UserID;
-            existingItem.LastUpdatedOnDate = DateTime.UtcNow;
-            existingItem.Title = item.Title;
-            existingItem.Description = item.Description;
+              <%= extensionName %>InfoRepository.Instance.CreateItem(item);
+          }
+          else
+          {
+              var existingItem = <%= extensionName %>InfoRepository.Instance.GetItem(item.<%= extensionName %>Id, item.ModuleId);
+              existingItem.LastUpdatedByUserId = User.UserID;
+              existingItem.LastUpdatedOnDate = DateTime.UtcNow;
+              existingItem.Title = item.Title;
+              existingItem.Description = item.Description;
 
-                    <%= extensionName %>InfoRepository.Instance.UpdateItem(existingItem);
-        }
+              <%= extensionName %>InfoRepository.Instance.UpdateItem(existingItem);
+          }
 
-        return RedirectToDefaultRoute();
-    }
+          return RedirectToDefaultRoute();
+      }
 
-    [ModuleAction(ControlKey = "Edit", TitleKey = "AddItem")]
-    public ActionResult Index()
-    {
-        var items = <%= extensionName %>InfoRepository.Instance.GetItems(ModuleContext.ModuleId);
-        return View(items);
-    }
+      [ModuleAction(ControlKey = "Edit", TitleKey = "AddItem")]
+      public ActionResult Index()
+      {
+          var items = <%= extensionName %>InfoRepository.Instance.GetItems(ModuleContext.ModuleId);
+          return View(items);
+      }
     }
 }
