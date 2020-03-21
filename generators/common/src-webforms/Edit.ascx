b8c87733eddb93eec4e0fe9699ecb8b18d62bc40
Gerald Ncube <gncube@gmail.com>
2020-03-21 12:34:48 +0000

    added edit code

--- generators/common/src-webforms/Edit.ascx
+++ generators/common/src-webforms/Edit.ascx
@@ -1,7 +1,35 @@
-﻿<<%= openDirective %> Control Language="C#" AutoEventWireup="true" CodeBehind="Edit.ascx.cs" Inherits="<%= fullNamespace %>.Edit" <%= closeDirective %>>
+<<%= openDirective %> Control Language="C#" AutoEventWireup="true" CodeBehind="Edit.ascx.cs" Inherits="<%= fullNamespace %>.Edit" <%= closeDirective %>>
 <<%= openDirective %> Import Namespace="DotNetNuke.Services.Localization" <%= closeDirective %>>
 <<%= openDirective %> Register TagPrefix="dnn" TagName="Label" Src="~/controls/labelcontrol.ascx" <%= closeDirective %>>
 
-<div class="dnnClear">
-    This is the edit view
-</div>
+  <div class="dnnForm">
+        <asp:Label runat="server" CssClass="dnnFormMessage dnnFormInfo" ResourceKey="Intro" />
+    <div class="dnnFormItem dnnFormHelp dnnClear">
+        <p class="dnnFormRequired"><asp:Label runat="server" ResourceKey="Required Indicator" /></p>
+    </div>
+    <fieldset>
+      <div class="dnnFormItem">
+          <dnn:label ID="lblTitle" runat="server" />
+          <asp:TextBox ID="txtTitle" CssClass="dnnFormRequired" runat="server" />
+          <asp:RequiredFieldValidator ID="rqValTitle" CssClass="dnnFormMessage dnnFormError" runat="server"
+              ControlToValidate="txtTitle" ErrorMessage="Title is required"></asp:RequiredFieldValidator>
+      </div>
+      <div class="dnnFormItem">
+          <dnn:label ID="lblDescription" runat="server" />
+          <asp:TextBox ID="txtDescription" CssClass="dnnFormRequired" runat="server" TextMode="MultiLine" Rows="3" />
+          <asp:RequiredFieldValidator ID="rqValDescription" CssClass="dnnFormMessage dnnFormError" runat="server"
+              ControlToValidate="txtDescription" ErrorMessage="Description is required"></asp:RequiredFieldValidator>
+      </div>
+
+      <ul class="dnnActions dnnClear">
+          <li>
+              <asp:LinkButton ID="btnSubmit" CssClass="dnnPrimaryAction" CausesValidation="true" onclick="btnSubmit_Click"
+                  runat="server">Save</asp:LinkButton>
+          </li>
+          <li>
+              <asp:LinkButton ID="btnCancel" CssClass="dnnSecondaryAction" CausesValidation="false" onclick="btnCancel_Click"
+                  runat="server">Cancel</asp:LinkButton>
+          </li>
+      </ul>
+    </fieldset>
+  </div>
--- generators/common/src-webforms/Edit.ascx.cs
+++ generators/common/src-webforms/Edit.ascx.cs
@@ -21,15 +21,79 @@ namespace <%= fullNamespace %>
             }
         }
 
-        #endregion
-
-        #region Helper Methods
-
-        private void BindData()
+        protected void Page_PreRender(object sender, EventArgs e)
         {
+            ClientResourceManager.RegisterStyleSheet(this.Page, base.ControlPath + "resources/css/module.css");
+        }
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var t = new ExampleInfo();
+            var tc = new ExampleInfoRepository();
+
+            if (ItemId > 0)
+            {
+                t = tc.GetItem(ItemId, ModuleId);
+                t.Title = txtTitle.Description.Trim();
+                t.Description = txtDescription.Text.Trim();
+            }
+            else
+            {
+                t = new ExampleInfo()
+                {
+                  CreatedByUserId = UserId,
+                  CreatedOnDate = DateTime.Now,
+                  Title = txtTitle.Text.Trim(),
+                  Description = txtDescription.Text.Trim(),
+
+                };
+            }
+
+            t.LastUpdatedOnDate = DateTime.Now;
+            t.LastUpdatedByUserId = UserId;
+            t.ModuleId = ModuleId;
+
+            if (t.ItemId > 0)
+            {
+                tc.UpdateItem(t);
+            }
+            else
+            {
+                tc.CreateItem(t);
+            }
+            Response.Redirect(DotNetNuke.Common.Globals.NavigateURL());
+        }
+
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+          Response.Redirect(DotNetNuke.Common.Globals.NavigateURL());
+        }
+
+  #endregion
+
+  #region Helper Methods
+
+  private void BindData()
+        {
+            if (!Page.IsPostBack)
+            {
+                if (ItemId > 0)
+                {
+                    var tc = new ExampleInfoRepository();
+
+                    var t = tc.GetItem(ItemId, ModuleId);
+                    if (t != null)
+                    {
+                        txtTitle.Text = t.Title;
+                        txtDescription.Text = t.Description;
+                    }
+                }
+            }
+
+
             LocalizeModule();
-			
-			
         }
 
         private void LocalizeModule()
