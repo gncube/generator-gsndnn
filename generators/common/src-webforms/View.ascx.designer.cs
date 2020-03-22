e28f6fb24351f6d2ced7fb2a11a8ec187515eb13
Gerald Ncube <gncube@gmail.com>
2020-03-22 07:02:10 +0000

    corrected database plural and view disegner code

--- generators/common/src-webforms/View.ascx.designer.cs
+++ generators/common/src-webforms/View.ascx.designer.cs
@@ -11,7 +11,14 @@ namespace <%= fullNamespace %> {
     
     
     public partial class View {
-        
 
+        /// <summary>
+        /// rptAnnouncementList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rpt<%= extensionName %>List;
     }
 }
