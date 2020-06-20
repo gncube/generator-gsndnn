c6f4aa098a8f15020852f6e8c22d3eb5389ca586
Gerald Ncube <gncube@gmail.com>
2020-06-20 14:29:19 +0100

    added PortalId and ContentItemId

--- generators/webforms/templates/Entities/ExampleInfo.cs
+++ generators/webforms/templates/Entities/ExampleInfo.cs
@@ -12,21 +12,15 @@ namespace <%= fullNamespace %>.Entities
     public class <%= extensionName %>Info : I<%= extensionName %>Info
     {
         public int <%= extensionName %>Id { get; set; }
-
+        public int PortalId { get; set; }
         public int ModuleId { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
-
+        public int ContentItemId { get; set; }
         public bool IsDeleted { get; set; }
-
         public int CreatedByUserId { get; set; }
-
         public DateTime CreatedOnDate { get; set; }
-
         public int LastUpdatedByUserId { get; set; }
-
         public DateTime LastUpdatedOnDate { get; set; }
     }
 }
